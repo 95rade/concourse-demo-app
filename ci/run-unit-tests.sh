85e88d66a039a4df7a18fd4b586f11ed5a2ac7c6
rmilij001c <rade95@gmail.com>
2019-04-02 11:42:43 -0600

    run as shell script

---
+++ ci/run-unit-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -xe
+
+export GOPATH=$PWD/go
+export PATH=$GOPATH/bin:$PATH
+pwd
+ls -ltr
+#go get github.com/tools/godep
+#go get golang.org/x/tools/cmd/cover
+#source_dir="$(cd "$(dirname "$0")" && pwd)"
+#cd $source_dir/..
+#godep go test ./... -v -cover -race
